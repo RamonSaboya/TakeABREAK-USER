4d2e8f9b56e90a89fac10d29ed742e7b3e407266
Ramon Saboya <rsg3@cin.ufpe.br>
2016-11-12 20:16:05 -0300

    Iniciar estrutura de projeto
    - Criar interface de repositório
    
    Signed-off-by: Ramon Saboya <rsg3@cin.ufpe.br>

---
+++ src/br/ufpe/cin/if678/business/User.java
@@ -0,0 +1,15 @@
+package br.ufpe.cin.if678.business;
+
+import java.util.Date;
+
+public class User {
+
+	private int ID;
+	private String username;
+	private String password;
+	private String email;
+	private Date birthdate;
+	private String fullName;
+	private String cellphone;
+	
+}
