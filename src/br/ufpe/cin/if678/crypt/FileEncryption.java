37c7a1e7f9a354ea5cbb8ac90904fb6cdad77915
João Filipe Moura <jfmrm@cin.ufpe.br>
2016-11-15 10:28:08 -0300

    FileEncryption comentado.
    Falta testar e escrever o arquivo com o nome correto.
    
    Signed-off-by: João Filipe Moura <jfmrm@cin.ufpe.br>

--- src/br/ufpe/cin/if678/crypt/FileEncryption.java
+++ src/br/ufpe/cin/if678/crypt/FileEncryption.java
@@ -48,35 +48,60 @@ public class FileEncryption {
 		return IV;
 	}
 	
+	/**
+	 * 
+	 * @param file
+	 * @return	FileEncryption
+	 * @throws IllegalBlockSizeException
+	 * @throws BadPaddingException
+	 * @throws IOException
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchProviderException
+	 * @throws NoSuchPaddingException
+	 * @throws InvalidKeyException
+	 * @throws InvalidAlgorithmParameterException
+	 * 
+	 * Encripta o arquivo de entrada, e devolve o tipo FileEncryption que coném o arquivo e o nome do arquivo encriptados, e a key e IV usados para encriptá-los
+	 */
 	public static FileEncryption encrypt(File file) throws IllegalBlockSizeException, BadPaddingException, IOException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException{
+		// Inicia Cipher para a criptografia AES
 		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
-
+		
+		// Gera um IV aleatório
 		SecureRandom secureRandom = new SecureRandom();
 		byte[] IV = new byte[cipher.getBlockSize()];
 		secureRandom.nextBytes(IV);
-
+		
+		// Gera uma chave secreta aleatórioa
 		KeyGenerator encriptKey = KeyGenerator.getInstance("AES");
 		encriptKey.init(128);
 		SecretKey key = encriptKey.generateKey();
 
+		// Inicia cipher para o medo de encriptação, com a SecretKey e IV gerados
 		cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(IV));
-
+		
+		// Encripta o arquivo, a partir do array de bytes
 		byte[] encryptedFile = cipher.doFinal(Files.readAllBytes(file.toPath()));
-			
+		
+		// Encripta o nome do arquivo
 		byte[] fileName = file.getName().getBytes();
 		byte[] encryptedFileName = cipher.doFinal(fileName);
 		
 		return new FileEncryption(encryptedFileName, encryptedFile, key, IV);
 	}
 	
-	public static File decrypt(FileEncryption encryptedFile, Path recivingPath) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException{
+	public static void decrypt(FileEncryption encryptedFile, Path writePath) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, IOException{
+		// Inicia cipher para o modo AES, de decriptação, com a SecretKey e IV do FileEncryption recebido
 		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
 		cipher.init(Cipher.DECRYPT_MODE, encryptedFile.getKey(), new IvParameterSpec(encryptedFile.getIV()));
 		
+		// Decripta o arquivo
 		byte[] decryptedFile = cipher.doFinal(encryptedFile.getFile());
 		
-		byte[] decryptedFileName = cipher.doFinal(encryptedFile.getFileName());
+		// Decripta o nome do arquivo
+		String decryptedFileName = cipher.doFinal(encryptedFile.getFileName()).toString();
 		
-		return new File()
+		// Escreve o arquivo no Path recebido.
+		Files.write(writePath, decryptedFile);
 	}
 }
