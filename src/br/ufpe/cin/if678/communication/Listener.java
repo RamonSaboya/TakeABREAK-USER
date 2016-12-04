f1241626421f75d93d51cc311ece54859cc95203
Ramon Saboya <rsg3@cin.ufpe.br>
2016-12-04 20:29:24 -0300

    Sincronizar
    Signed-off-by: Ramon Saboya <rsg3@cin.ufpe.br>

--- src/br/ufpe/cin/if678/communication/Listener.java
+++ src/br/ufpe/cin/if678/communication/Listener.java
@@ -40,6 +40,8 @@ public class Listener {
 	public void onGroupReceive(Group group) {
 		controller.getGroups().put(group.getName(), group);
 
+		System.out.println("Recebendo grupo");
+
 		TakeABREAK.getInstance().getChatListPanel().updateChatList();
 	}
 
