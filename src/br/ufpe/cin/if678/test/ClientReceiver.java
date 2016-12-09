da650a6a5192f3a7838a94d4f65439231c0dd071
João Filipe Moura <jfmrm@cin.ufpe.br>
2016-12-09 08:42:54 -0300

    Signed-off-by: João Filipe Moura <jfmrm@cin.ufpe.br>

--- src/br/ufpe/cin/if678/test/ClientReceiver.java
+++ src/br/ufpe/cin/if678/test/ClientReceiver.java
@@ -1,5 +1,6 @@
 package br.ufpe.cin.if678.test;
 
+import java.io.DataInputStream;
 import java.io.FileOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -20,13 +21,19 @@ public class ClientReceiver {
 			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
 			cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec("1234567812345678".getBytes("UTF-8"), "AES"), new IvParameterSpec("masi129fapwQMV0W".getBytes()));
 
+			DataInputStream DIS = new DataInputStream(socket.getInputStream());
+			long fileSize = DIS.readLong();
+			
 			CipherInputStream CIS = new CipherInputStream(socket.getInputStream(), cipher);
 			FileOutputStream FOS = new FileOutputStream("received.rar");
 
 			byte[] buffer = new byte[4 * 1024];
 
 			int count;
+			long downloaded = 0;
 			while ((count = CIS.read(buffer)) > 0) {
+				downloaded += count;
+				System.out.println("DOWNLOADED: " + (downloaded/fileSize));
 				FOS.write(buffer, 0, count);
 			}
 
--- src/br/ufpe/cin/if678/test/ClientSender.java
+++ src/br/ufpe/cin/if678/test/ClientSender.java
@@ -1,5 +1,6 @@
 package br.ufpe.cin.if678.test;
 
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.net.Socket;
@@ -16,17 +17,26 @@ public class ClientSender {
 			Socket socket = new Socket("localhost", 4848);
 
 			File file = new File("origin.rar");
-
+			
 			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
 			cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec("1234567812345678".getBytes("UTF-8"), "AES"), new IvParameterSpec("masi129fapwQMV0W".getBytes()));
 
 			FileInputStream FIS = new FileInputStream(file);
+			
+			// Envia tamanho do arquivo a ser enviado
+			DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());
+			long fileSize = file.length();
+			DOS.writeLong(fileSize);
+			
 			CipherOutputStream COS = new CipherOutputStream(socket.getOutputStream(), cipher);
 
 			byte[] buffer = new byte[4 * 1024];
 
 			int count;
+			long uploaded = 0;
 			while ((count = FIS.read(buffer)) > 0) {
+				uploaded += count;
+				System.out.println(uploaded/fileSize);
 				COS.write(buffer, 0, count);
 			}
 
